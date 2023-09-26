44f5fd9e50f0f950f94425ffd184941a12549c83
amani esseili <esseiliamani@hotmail.com>
2023-09-26 22:29:25 +0200

    foreach che nn funziona

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie; // importare il model movie
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+
+    public function index() {
+
+        $dati = Movie::all(); // recuperare tutti i film dal database
+
+        // dd($dati);
+
+
+        return view('home', [
+            "Movies" => $dati
+        ]);
+
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.public')
 
 @section("content")
-<h1>hello world</h1>
+<div class="container">
+  <div class="row">
+      @foreach ($movies as $movie)
+          <div class="col-md-4">
+              <div class="card">
+                  <div class="card-body">
+                      <h5 class="card-title">{{ $movie->title }}</h5>
+                      <p class="card-text">{{ $movie->nationality }}</p>
+                      <p class="card-text">{{ $movie->date}}</p>
+                      <p class="card-text">{{ $movie->vote}}</p>
+                      
+                  </div>
+              </div>
+          </div>
+      @endforeach
+  </div>
+</div>
 @endsection
